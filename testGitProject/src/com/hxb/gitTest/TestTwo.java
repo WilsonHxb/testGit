5d31c65f428ad2362ba914b057968bb200a411ae
WilsonHxb <106042097@qq.com>
2018-04-17 15:40:53 +0800

    sssss

--- testGitProject/src/com/hxb/gitTest/TestTwo.java
+++ testGitProject/src/com/hxb/gitTest/TestTwo.java
@@ -3,7 +3,7 @@ package com.hxb.gitTest;
 public class TestTwo {
 	
 	public static void main(String[] args) {
-		System.out.println("test github two");
+		System.out.println("test github two. lala");
 	}
 
 }

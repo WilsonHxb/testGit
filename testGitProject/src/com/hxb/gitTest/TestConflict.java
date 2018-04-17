93274063aae7513db6aaa504304444203348ce3f
WilsonHxb <106042097@qq.com>
2018-04-17 16:18:52 +0800

    Update TestConflict.java
    conflict 3 on github

--- testGitProject/src/com/hxb/gitTest/TestConflict.java
+++ testGitProject/src/com/hxb/gitTest/TestConflict.java
@@ -3,7 +3,7 @@ package com.hxb.gitTest;
 public class TestConflict {
 
 	public static void main(String[] args) {
-		System.out.println("test conflict....321psfa");
+		System.out.println("test conflict....321psfasdfadfaafsdfasdfadfadfasfasdfasdfasdfas");
 	}
 
 }

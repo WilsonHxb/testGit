6018d386d1c6d11b6a1fbf5334ce4798f2ff8cc2
huxb <huxubin@pcidata.cn>
2018-04-18 17:14:42 +0800

    22222

--- testGitProject/src/com/hxb/gitTest/TestConflict.java
+++ testGitProject/src/com/hxb/gitTest/TestConflict.java
@@ -3,7 +3,7 @@ package com.hxb.gitTest;
 public class TestConflict {
 
 	public static void main(String[] args) {
-		System.out.println("A... net edited 1");
+		System.out.println("A... net edited 132");
 		System.out.print("B...");
 		System.out.print("C...eclipse edited 1");
 	}

25e9e247620cb45bc7335e09c05636cec55cc323
huxb <huxubin@pcidata.cn>
2018-04-18 16:42:13 +0800

    test conflict 2

--- testGitProject/src/com/hxb/gitTest/TestConflict.java
+++ testGitProject/src/com/hxb/gitTest/TestConflict.java
@@ -5,7 +5,7 @@ public class TestConflict {
 	public static void main(String[] args) {
 		System.out.println("A...");
 		System.out.print("B...");
-		System.out.print("C...");
+		System.out.print("C...eclipse edited");
 	}
 
 }

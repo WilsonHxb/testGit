25d51b0e5a5ba8f385b8b8d582f06aa9b7662b1f
huxb <huxubin@pcidata.cn>
2018-06-04 11:23:11 +0800

    ccc

--- testGitProject/src/com/hxb/gitTest/TestOne.java
+++ testGitProject/src/com/hxb/gitTest/TestOne.java
@@ -4,6 +4,7 @@ public class TestOne {
 	
 	public static void main(String[] args) {
 		System.out.println("hello github one, edit by browser. add file.");
+		System.out.println("add by eclipse.");
 	}
 
 }
